cbded0419bb334f506a54c6d7eb4d6509abaf9e9
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2022-12-23 18:40:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sleep sort - the lazy man's sort
+This repository contains a modern C++ implementation of the Sleep Sort algorithm, with proper comments.
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <format>
+
+
+void SleepAndPrint(int n) {
+    // replace seconds with minutes for maximum efficiency
+    std::this_thread::sleep_for(std::chrono::seconds(n));
+    // C++ now has a format() function similar to Python
+    std::cout << std::format("{}, ", n);
+}
+
+int main() {
+    // modify at your own risk
+    std::vector<int> nums{1, 4, 10, 7, 2, 18, 3, 0};
+
+    // a vector to store threads
+    std::vector<std::thread> threads;
+
+    for (int n: nums) {
+        // spawn a new thread that will run SleepAndPrint with arguement n
+        std::thread t{SleepAndPrint, n};
+        // threads can only be moved, and not copied
+        threads.push_back(std::move(t));
+    }
+
+    // now join all the threads
+    for (auto &t: threads) {
+        // A thread may already be done by the time we get here, so check for that first
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+    std::cout << "\nSorting complete.";
+
+    return 0;
+}
